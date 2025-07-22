e00daa1e7b931d842956e8bd1c2fbe7d5e521be5
CW-ISM <cwismgitism@gmail.com>
2025-07-21 18:25:22 -0700

    Add Printer3D class with required properties and constructor
    Introduces a new `Printer3D` class in `Printer3D.cs` that includes required properties: `BuildVolume`, `SupportedFilaments`, `ProductName`, `Cost`, and `ReleaseDate`. A constructor is provided to initialize these properties upon instantiation.

---
+++ Printer3D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Printer3D
+{
+	public required float BuildVolume { get; set; }
+    public required string[] SupportedFilaments { get; set; }
+    public required string ProductName { get; set; }
+    public required float Cost { get; set; }
+	public required DateTime ReleaseDate { get; set; }
+
+    public Printer3D(
+		float buildVolume, string[] supportedFilaments,
+		string productName, float cost, DateTime releaseDate)
+	{
+		BuildVolume = buildVolume;
+        SupportedFilaments = supportedFilaments;
+        ProductName = productName;
+        Cost = cost;
+        ReleaseDate = releaseDate;
+    }
+}
